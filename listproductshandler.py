0c8a68adc7198eb1ba876b1948f93bb6591a8e89
Szilard Huber <szilard.huber@gmail.com>
2012-12-29 16:56:28 +0100

    Adding django templating to list products page #2

--- listproductshandler.py
+++ listproductshandler.py
@@ -5,6 +5,8 @@ from action import *
 from google.appengine.ext import db
 import logging
 import webapp2
+import os
+from google.appengine.ext.webapp import template
 
 class ListProductsHandler(webapp2.RequestHandler):
 	def get(self):
@@ -15,6 +17,12 @@ class ListProductsHandler(webapp2.RequestHandler):
                             "WHERE ANCESTOR IS :1 "
                             "ORDER BY date DESC LIMIT 10",
                             user_key(user_guid))
-                for action in actions:
-                	self.response.out.write('<b>%s</b><br>' % action.barcode)
+                #for action in actions:
+                #	self.response.out.write('<b>%s</b><br>' % action.barcode)
+                template_values = {
+                	'guid' : user_guid,
+                	'actions': actions
+                }
+                path = os.path.join(os.path.dirname(__file__), 'templates/productlist.html')
+                self.response.out.write(template.render(path, template_values))
 
---
+++ settings.py
@@ -0,0 +1,8 @@
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'google.appengine.ext.ndb.django_middleware.NdbDjangoMiddleware'
+)
