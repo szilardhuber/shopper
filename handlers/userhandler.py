15eb3f72129a919182a8e864c27a50738cfcec38
Szilárd Huber <szhuber@Szilard-Hubers-MacBook-Pro-3.local>
2013-02-21 11:15:34 +0100

    #SH-7 'in progress'

--- handlers/userhandler.py
+++ handlers/userhandler.py
@@ -7,7 +7,7 @@ from utilities import usercallable
 from utilities import viewneeded
 from basehandler import BaseHandler
 
-# comment for testing youtrack integration 7
+# comment for testing youtrack integration 8
 
 # External libraries
 from gaesessions import get_current_session
