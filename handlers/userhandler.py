3a035163b1369d696b3804685fb914f3d4ebaed4
Szilárd Huber <szhuber@Szilard-Hubers-MacBook-Pro-3.local>
2013-02-21 11:06:23 +0100

    Still testing integration #SH-7 'In Progress' 'assignee me'

--- handlers/userhandler.py
+++ handlers/userhandler.py
@@ -7,7 +7,7 @@ from utilities import usercallable
 from utilities import viewneeded
 from basehandler import BaseHandler
 
-# comment for testing youtrack integration
+# comment for testing youtrack integration 2
 
 # External libraries
 from gaesessions import get_current_session
