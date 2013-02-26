302cf039da998df8cfd2d67f2f85d78b46820e4a
Szilárd Huber <szhuber@Szilard-Hubers-MacBook-Pro-3.local>
2013-02-26 21:05:32 +0100

    #SH-7 state in progress

--- handlers/userhandler.py
+++ handlers/userhandler.py
@@ -7,7 +7,7 @@ from utilities import usercallable
 from utilities import viewneeded
 from basehandler import BaseHandler
 
-# another round with you track integration 4
+# another round with you track integration 5
 
 # External libraries
 from gaesessions import get_current_session
