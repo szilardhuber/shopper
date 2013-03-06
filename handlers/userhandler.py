35b79cc1da9c25f671cc4745fda051589495cc70
Szilárd Huber <szhuber@Szilard-Hubers-MacBook-Pro-3.local>
2013-03-06 15:50:31 +0100

    Still testing build server

--- handlers/userhandler.py
+++ handlers/userhandler.py
@@ -7,7 +7,7 @@ from utilities import usercallable
 from utilities import viewneeded
 from basehandler import BaseHandler
 
-# another round with you track integration 7
+# another round with you track integration 8
 
 # External libraries
 from gaesessions import get_current_session
