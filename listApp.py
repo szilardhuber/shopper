96e1da6a6516810a71d2ea3ba767a776ed1dc0b2
Szilárd Huber <szhuber@Szilard-Hubers-MacBook-Pro-3.local>
2013-03-06 09:18:14 +0100

    Create stub for #SH-55 in listApp

--- listApp.py
+++ listApp.py
@@ -5,6 +5,17 @@ from listhandler import ListHandler
 # libraries
 import webapp2
 from webapp2 import Route
+import logging
+                
+def handle_404(request, response, exception):
+    logging.exception(exception)
+    response.write('Oops! I could swear this page was here!')
+    response.set_status(404)
+    
+def handle_500(request, response, exception):
+    logging.exception(exception)
+    response.write('A server error occurred!')
+    response.set_status(500)
                 
 app = webapp2.WSGIApplication([
 				Route('/<api:api>/v1/Lists/', handler=ListHandler),
@@ -21,3 +32,6 @@ app = webapp2.WSGIApplication([
 				Route('/lists/<list_id>', handler=ListHandler)
 				],
                               debug=True)
+
+app.error_handlers[404] = handle_404
+app.error_handlers[500] = handle_500
