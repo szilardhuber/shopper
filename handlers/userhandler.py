fce005e9ad830a2f9814fd79faa2fde874ca5900
Szilárd Huber <szhuber@Szilard-Hubers-MacBook-Pro-3.local>
2013-03-06 16:28:33 +0100

    Testing integration #SH-7 in progress

--- handlers/userhandler.py
+++ handlers/userhandler.py
@@ -7,7 +7,7 @@ from utilities import usercallable
 from utilities import viewneeded
 from basehandler import BaseHandler
 
-# another round with you track integration 8
+# another round with you track integration 9
 
 # External libraries
 from gaesessions import get_current_session
