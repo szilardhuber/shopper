3dd885d37e466b307b2f461b7eb73c2506bcbe7a
Szilárd Huber <szhuber@Szilard-Hubers-MacBook-Pro-3.local>
2013-03-12 14:11:01 +0100

    #SH-56 done

--- test/test_user.py
+++ test/test_user.py
@@ -2,6 +2,7 @@
 from userhandler import UserHandler
 from listproductshandler import ListProductsHandler
 from utilities import constants
+from user_util import UserUtil
 
 # libraries, builtins
 from google.appengine.api import mail
@@ -12,12 +13,6 @@ import webtest
 from gaesessions import get_current_session
 
 class WebTest_User(unittest.TestCase):
-	verifyPrefix = 'https://szilardhuber.appspot.com/User/Verify?code='
-	loginURL = '/User/Login/API'
-	registerURL = '/User/Register/API'
-	verifyURL = '/User/Verify'
-	logoutURL = '/User/Logout/api'
-	
 	def setUp(self):
 		self.testbed = testbed.Testbed()
 		self.testbed.activate()
@@ -32,57 +27,57 @@ class WebTest_User(unittest.TestCase):
 		self.testapp = webtest.TestApp(app)
 	
 	def testRegisterHandlerEmpty(self):
-		response = self.__register_user()
+		response = UserUtil.register_user(self.testapp)
 		self.assertEqual(response.status_int, constants.STATUS_BAD_REQUEST, 'Code constants.STATUS_BAD_REQUEST should arrive when calling without parameters: ' + str(response.status_int))
 		
 	def testRegisterHandlerWrongEmail(self):
-		response = self.__register_user('jamesbond.com', 'password')
+		response = UserUtil.register_user(self.testapp, 'jamesbond.com', 'password')
 		self.assertEqual(response.status_int, constants.STATUS_BAD_REQUEST, 'Code constants.STATUS_BAD_REQUEST should arrive when calling with bad email: ' + str(response.status_int))
-		response = self.__register_user('james@com', 'password')
+		response = UserUtil.register_user(self.testapp, 'james@com', 'password')
 		self.assertEqual(response.status_int, constants.STATUS_BAD_REQUEST, 'Code constants.STATUS_BAD_REQUEST should arrive when calling with bad email: ' + str(response.status_int))
-		response = self.__register_user('@bond.com', 'password')
+		response = UserUtil.register_user(self.testapp, '@bond.com', 'password')
 		self.assertEqual(response.status_int, constants.STATUS_BAD_REQUEST, 'Code constants.STATUS_BAD_REQUEST should arrive when calling with bad email: ' + str(response.status_int))
 		
 	def testRegisterHandlerWeakPassword(self):
-		response = self.__register_user('james@bond.com', '1')
+		response = UserUtil.register_user(self.testapp, 'james@bond.com', '1')
 		self.assertEqual(response.status_int, constants.STATUS_BAD_REQUEST, 'Code constants.STATUS_BAD_REQUEST should arrive when calling with too weak password: ' + str(response.status_int))
-		response = self.__register_user('james@bond.com')
+		response = UserUtil.register_user(self.testapp, 'james@bond.com')
 		self.assertEqual(response.status_int, constants.STATUS_BAD_REQUEST, 'Code constants.STATUS_BAD_REQUEST should arrive when calling without password: ' + str(response.status_int))
 		
 	def testRegisterHandlerBadPassword(self):
 		email = 'james@aisoft.hu'
 		password = '"a"@a.hu'
-		response = self.__register_user(email, password)
+		response = UserUtil.register_user(self.testapp, email, password)
 		self.assertEqual(response.status_int, constants.STATUS_BAD_REQUEST, 'Code constants.STATUS_BAD_REQUEST should arrive when calling with invalid characters in password: ' + password + ' ' + str(response.status_int))
 		password = 	'"a;"@a.hu'
-		response = self.__register_user(email, password)
+		response = UserUtil.register_user(self.testapp, email, password)
 		self.assertEqual(response.status_int, constants.STATUS_BAD_REQUEST, 'Code constants.STATUS_BAD_REQUEST should arrive when calling with invalid characters in password: ' + password + ' ' + str(response.status_int))
 		password = "a'a@a.hu"	
-		response = self.__register_user(email, password)
+		response = UserUtil.register_user(self.testapp, email, password)
 		self.assertEqual(response.status_int, constants.STATUS_BAD_REQUEST, 'Code constants.STATUS_BAD_REQUEST should arrive when calling with invalid characters in password: ' + password + ' ' + str(response.status_int))
 		password = "a;a@a.hu"
-		response = self.__register_user(email, password)
+		response = UserUtil.register_user(self.testapp, email, password)
 		self.assertEqual(response.status_int, constants.STATUS_BAD_REQUEST, 'Code constants.STATUS_BAD_REQUEST should arrive when calling with invalid characters in password: ' + password + ' ' + str(response.status_int))
 	
 	def testRegisterHandlerOK(self):
-		response = self.__register_user('james@bond.com','password')
+		response = UserUtil.register_user(self.testapp, 'james@bond.com','password')
 		self.assertEqual(response.status_int, constants.STATUS_OK, 'Wrong response with correct credentials: ' + str(response.status_int))
-		response = self.__register_user('admin123254334343434234234@domain12312312434324.hu', 'password')
+		response = UserUtil.register_user(self.testapp, 'admin123254334343434234234@domain12312312434324.hu', 'password')
 		self.assertEqual(response.status_int, constants.STATUS_OK, 'Wrong response with correct credentials: ' + str(response.status_int))
 		
 	def testRegisteringTwice(self):
-		response = self.__register_user('james@bond.com', 'password')
+		response = UserUtil.register_user(self.testapp, 'james@bond.com', 'password')
 		self.assertEqual(response.status_int, constants.STATUS_OK, 'Wrong response with correct credentials: ' + str(response.status_int))
-		response = self.__register_user('james@bond.com', 'password')
+		response = UserUtil.register_user(self.testapp, 'james@bond.com', 'password')
 		self.assertEqual(response.status_int, constants.STATUS_BAD_REQUEST, 'Wrong response for second registration: ' + str(response.status_int))
-		response = self.__register_user('jAmes@bond.com', 'password')
+		response = UserUtil.register_user(self.testapp, 'jAmes@bond.com', 'password')
 		self.assertEqual(response.status_int, constants.STATUS_BAD_REQUEST, 'System is case sensitive for emails: ' + str(response.status_int))
 		
 	def testLoginSuccess(self):
 		email = 'james@bond.com'
 		password = 'password'
 		# 1. Register client
-		response = self.__register_user(email, password)
+		response = UserUtil.register_user(self.testapp, email, password)
 		self.assertEqual(response.status_int, constants.STATUS_OK, 'Register failed with correct credentials: ' + str(response.status_int))
 		
 		# 2. Access test site - error should arrive
@@ -90,11 +85,11 @@ class WebTest_User(unittest.TestCase):
 		self.assertEqual(response.status_int, constants.STATUS_UNAUTHORIZED, 'Users only page should be served after logging in: ' + str(response.status_int))
 
 		# 3. Try to login -> Verification needed first
-		response = self.__login_user(email, password)
+		response = UserUtil.login_user(self.testapp, email, password)
 		self.assertEqual(response.status_int, constants.STATUS_FORBIDDEN, 'Server should answer 403 for unverified client: ' +str(response.status_int))
 
 		# 4. Verify
-		response = self.__verify_user(email)
+		response = UserUtil.verify_user(self.testapp, self.mail_stub, email)
 		self.assertEqual(response.status_int, constants.STATUS_OK, 'Verification failed: '+ str(response.status_int))
 		
 		# 5. Access test site should succeed after verification
@@ -111,23 +106,23 @@ class WebTest_User(unittest.TestCase):
 		self.assertEqual(response.status_int, constants.STATUS_OK, 'Users only page should be served after logging in: ' + str(response.status_int))
 		
 		# 8. Logout
-		response = self.testapp.get(self.logoutURL, expect_errors=True)
+		response = UserUtil.logout(self.testapp)
 		self.assertEqual(response.status_int, constants.STATUS_OK, 'Logout failed: ' + str(response.status_int))
 		
 		# 9. SH-26 regression
-		response = self.testapp.get(self.logoutURL, expect_errors=True)
+		response = UserUtil.logout(self.testapp)
 		self.assertEqual(response.status_int, constants.STATUS_OK, 'Logout failed: ' + str(response.status_int))
 		
 	def testLoginFailWithoutVerification(self):
 		email = 'james@bond.com'
 		password = 'password'
-		response = self.__login_user(email, password)
+		response = UserUtil.login_user(self.testapp, email, password)
 		self.assertEqual(response.status_int, constants.STATUS_BAD_REQUEST, 'Login succeeded with empty db: ' + str(response.status_int))
-		response = self.__register_user(email, password)
+		response = UserUtil.register_user(self.testapp, email, password)
 		self.assertEqual(response.status_int, constants.STATUS_OK, 'Register failed with correct credentials: ' + str(response.status_int))
-		response = self.__login_user(email, 'password2')
+		response = UserUtil.login_user(self.testapp, email, 'password2')
 		self.assertEqual(response.status_int, constants.STATUS_BAD_REQUEST, 'Login succeeded with bad password: ' + str(response.status_int))
-		response = self.__login_user('james2@bond.com', password)
+		response = UserUtil.login_user(self.testapp, 'james2@bond.com', password)
 		self.assertEqual(response.status_int, constants.STATUS_BAD_REQUEST, 'Login succeeded with bad email: ' + str(response.status_int))
 
 	def testLoginFailWithVerification(self):
@@ -135,25 +130,25 @@ class WebTest_User(unittest.TestCase):
 		password = 'password'
 		
 		# 1. Register client
-		response = self.__login_user(email, password)
+		response = UserUtil.login_user(self.testapp, email, password)
 		self.assertEqual(response.status_int, constants.STATUS_BAD_REQUEST, 'Login succeeded with empty db: ' + str(response.status_int))
-		response = self.__register_user(email, password)
+		response = UserUtil.register_user(self.testapp, email, password)
 		self.assertEqual(response.status_int, constants.STATUS_OK, 'Register failed with correct credentials: ' + str(response.status_int))
 		
 		# 2. Verify client
-		response = self.__verify_user(email)
+		response = UserUtil.verify_user(self.testapp, self.mail_stub, email)
 		self.assertEqual(response.status_int, constants.STATUS_OK, 'Verification failed: '+ str(response.status_int))
 
 		# 3. Logout
-		response = self.testapp.get(self.logoutURL, expect_errors=True)
+		response = UserUtil.logout(self.testapp)
 		self.assertEqual(response.status_int, constants.STATUS_OK, 'Logout failed: ' + str(response.status_int))
 
 		# 4. Login with bad credentials
-		response = self.__login_user(email, 'password2')		
+		response = UserUtil.login_user(self.testapp, email, 'password2')		
 		self.assertEqual(response.status_int, constants.STATUS_BAD_REQUEST, 'Login succeeded with bad password.')
-		response = self.__login_user(email, '')		
+		response = UserUtil.login_user(self.testapp, email, '')		
 		self.assertEqual(response.status_int, constants.STATUS_BAD_REQUEST, 'Login succeeded with empty password.')
-		response = self.__login_user('james2@bond.com', password)
+		response = UserUtil.login_user(self.testapp, 'james2@bond.com', password)
 		self.assertEqual(response.status_int, constants.STATUS_BAD_REQUEST, 'Login succeeded with bad email.')
 		
 	def testPersistentCookie(self):
@@ -161,15 +156,15 @@ class WebTest_User(unittest.TestCase):
 		password = '12345678'
 		
 		# 1. Register client 
-		response = self.__register_user(email, password)
+		response = UserUtil.register_user(self.testapp, email, password)
 		self.assertEqual(response.status_int, constants.STATUS_OK, 'Register failed with correct credentials: ' + str(response.status_int))
 		
 		# 2. Verify client
-		response = self.__verify_user(email)
+		response = UserUtil.verify_user(self.testapp, self.mail_stub, email)
 		self.assertEqual(response.status_int, constants.STATUS_OK, 'Verification failed: '+ str(response.status_int))
 		
 		# 3. Login with remember me turned off
-		response = self.__login_user(email, password)
+		response = UserUtil.login_user(self.testapp, email, password)
 		self.assertEqual(response.status_int, constants.STATUS_OK, 'Login failed with verified client: ' + str(response.status_int))
 		
 		# 4. Acessing secure content (after login and after deleting session data)
@@ -181,7 +176,7 @@ class WebTest_User(unittest.TestCase):
 		self.assertEqual(response.status_int, constants.STATUS_UNAUTHORIZED, 'Users only page should not be served without providing session data: ' + str(response.status_int))
 		
 		# 5. Login with remember me turned on
-		response = self.__login_user(email, password, True)
+		response = UserUtil.login_user(self.testapp, email, password, True)
 		self.assertEqual(response.status_int, constants.STATUS_OK, 'Login failed with verified client: ' + str(response.status_int))
 		
 		# 6. Acessing secure content (after login and after deleting session data)
@@ -203,7 +198,7 @@ class WebTest_User(unittest.TestCase):
 		self.assertEqual(response.status_int, constants.STATUS_OK, 'Users only page should be served after logging in: ' + str(response.status_int))
 		
 		# 8. Logout
-		response = self.testapp.get(self.logoutURL, expect_errors=True)
+		response = UserUtil.logout(self.testapp)
 		self.assertEqual(response.status_int, constants.STATUS_OK, 'Logout failed: ' + str(response.status_int))
 		
 		# 9. Check logout
@@ -216,15 +211,15 @@ class WebTest_User(unittest.TestCase):
 		bad_password = 'wronwrongpassword'
 		
 		# 1. Register client 
-		response = self.__register_user(email, good_password)
+		response = UserUtil.register_user(self.testapp, email, good_password)
 		self.assertEqual(response.status_int, constants.STATUS_OK, 'Register failed with correct credentials: ' + str(response.status_int))
 		
 		# 2. Verify client
-		response = self.__verify_user(email)
+		response = UserUtil.verify_user(self.testapp, self.mail_stub, email)
 		self.assertEqual(response.status_int, constants.STATUS_OK, 'Verification failed: '+ str(response.status_int))
 		
 		# 3. Logout client
-		response = self.testapp.get(self.logoutURL, expect_errors=True)
+		response = UserUtil.logout(self.testapp)
 		self.assertEqual(response.status_int, constants.STATUS_OK, 'Logout failed: ' + str(response.status_int))
 		
 		# 4. Check logout
@@ -232,32 +227,9 @@ class WebTest_User(unittest.TestCase):
 		self.assertEqual(response.status_int, constants.STATUS_UNAUTHORIZED, 'Users only page should not be served after logout: ' + str(response.status_int))
 
 		# 5. Login with remember me turned on and a wrong password
-		response = self.__login_user(email, bad_password, True)
+		response = UserUtil.login_user(self.testapp, email, bad_password, True)
 		self.assertEqual(response.status_int, constants.STATUS_BAD_REQUEST, 'Login succeeded with bad password.' + str(response.status_int))
 		
 		# 6. Acessing secure content (after login and after deleting session data)
 		response = self.testapp.get('/api', expect_errors=True)
 		self.assertEqual(response.status_int, constants.STATUS_UNAUTHORIZED, 'Users only page must not be served without logging in: ' + str(response.status_int))
-
-	def __register_user(self, email = None, password = None):
-		params = {}
-		if email is not None:
-			params[constants.VAR_NAME_EMAIL] = email
-		if password is not None:
-			params[constants.VAR_NAME_PASSWORD] = password
-		response = self.testapp.post(self.registerURL, params, expect_errors=True)
-		return response
-	
-	def __verify_user(self, email):
-		messages = self.mail_stub.get_sent_messages(to=email)
-		self.assertEqual(len(messages), 1, "Exactly one message should have been sent to james@bond.com")
-		body = messages[0].body.decode()
-		url = body[body.find(self.verifyPrefix):]
-		code = url[len(self.verifyPrefix):url.find('\n')]
-		response = self.testapp.get(self.verifyURL, params={'code' : code}, expect_errors=True)
-		return response
-	
-	def __login_user(self, email, password, remember = False):
-		response = self.testapp.post(self.loginURL, params={constants.VAR_NAME_EMAIL : email, constants.VAR_NAME_PASSWORD : password, 'remember' : remember}, expect_errors=True)
-		return response
-
---
+++ test/user_util.py
@@ -0,0 +1,47 @@
+from webapp2 import Route
+from userhandler import UserHandler
+from utilities import constants
+
+class UserUtil():
+	LOGIN_URL = '/User/Login/API'
+	REGISTER_URL = '/User/Register/API'
+	VERIFY_URL = '/User/Verify'
+	LOGOUT_URL = '/User/Logout/api'
+	VERIFY_PREFIX = 'https://szilardhuber.appspot.com/User/Verify?code='
+
+	@staticmethod
+	def decorate_app(app):
+		app.router.add(('/[uU]ser/(.*)/(.*)/', UserHandler))
+		app.router.add(('/[uU]ser/(.*)/(.*)', UserHandler))
+		app.router.add(('/[uU]ser/(.*)', UserHandler))
+
+	@staticmethod
+	def register_user(testapp, email = None, password = None):
+		params = {}
+		if email is not None:
+			params[constants.VAR_NAME_EMAIL] = email
+		if password is not None:
+			params[constants.VAR_NAME_PASSWORD] = password
+		response = testapp.post(UserUtil.REGISTER_URL, params, expect_errors=True)
+		return response
+	
+	@staticmethod
+	def verify_user(testapp, mail_stub, email):
+		messages = mail_stub.get_sent_messages(to=email)
+		body = messages[0].body.decode()
+		url = body[body.find(UserUtil.VERIFY_PREFIX):]
+		code = url[len(UserUtil.VERIFY_PREFIX):url.find('\n')]
+		response = testapp.get(UserUtil.VERIFY_URL, params={'code' : code}, expect_errors=True)
+		return response
+	
+	@staticmethod
+	def login_user(testapp, email, password, remember = False):
+		response = testapp.post(UserUtil.LOGIN_URL, params={constants.VAR_NAME_EMAIL : email, constants.VAR_NAME_PASSWORD : password, 'remember' : remember}, expect_errors=True)
+		return response
+	
+	@staticmethod
+	def logout(testapp):
+		response = testapp.get(UserUtil.LOGOUT_URL, expect_errors=True)
+		return response
+		
+
