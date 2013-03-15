1728f833ab285fd903b15fe33dcec22c5fbc030d
Szilárd Huber <szhuber@Szilard-Hubers-MacBook-Pro-3.local>
2013-03-15 16:51:01 +0100

    And again...

--- model/__init__.py
+++ model/__init__.py
@@ -2,5 +2,5 @@ from user import User
 from action import Action
 from logintoken import LoginToken
 from sessiondata import SessionData
-from shoppinglist import ShopoingList
+from shoppinglist import ShoppingList
 from listitem import ListItem
