a8f36124d058adfb0872f7bf3f846f9876f62f00
szilardhuber <szilard.huber@gmail.com>
2013-04-23 17:31:06 +0300

    Update utils.py

--- utilities/utils.py
+++ utilities/utils.py
@@ -1,17 +1,19 @@
 import json
 
 def to_JSON(vect):
-	ret = ''
-	ret += '['
-	first = True 
-	for item in vect:
-		if not first:
-			ret += ', '
-		else:
-			first = False
-		try:
-			ret += json.dumps(item.to_dict())
-		except AttributeError:
-			ret += json.dumps(item)
-	ret += ']'
-	return ret
+    ret = ''
+    ret += '['
+    first = True 
+    for item in vect:
+        import logging
+        logging.info('Adding item: ' + dir(item))
+        if not first:
+	    ret += ', '
+        else:
+            first = False
+        try:
+	    ret += json.dumps(item.to_dict())
+        except AttributeError:
+	    ret += json.dumps(item)
+    ret += ']'
+    return ret
